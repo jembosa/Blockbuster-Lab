285beb9ab3188e528d3b324a258c73bda86e0b24
Alex Young <alexanderjyoung15@gmail.com>
2023-10-17 14:35:00 -0400

    Add project files.

---
+++ Blockbuster Lab/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace Blockbuster_Lab
+{
+    internal class Movie
+    {
+
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+    }
+}
---
+++ Blockbuster Lab/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Blockbuster_Lab
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Movie> movies = new List<Movie>
+        {
+                //10 movies and categories
+            new Movie("The Lion King", "Animated"),
+            new Movie("The Shawshank Redemption", "Drama"),
+            new Movie("The Shining", "Horror"),
+            new Movie("Blade Runner", "Sci-Fi"),
+            new Movie("Toy Story", "Animated"),
+            new Movie("Inception", "Sci-Fi"),
+            new Movie("The Godfather", "Drama"),
+            new Movie("Alien", "Sci-Fi"),
+            new Movie("Finding Nemo", "Animated"),
+            new Movie("Psycho", "Horror")
+        };
+            //welcome message
+            Console.WriteLine("Welcome to the very niche BlockBuster!");
+            Console.WriteLine($"There are {movies.Count} movies in the list");
+
+            while (true)
+            //Start loop
+            {
+                Console.Write("Enter a category for some suggestions! (Animated, Drama, Horror, Sci-Fi): ");
+                string userCategory = Console.ReadLine().ToLower();
+
+                bool categoryFound = false;
+
+                foreach (var movie in movies)
+                {
+                    if (movie.Category.ToLower() == userCategory)
+                    {
+                        Console.WriteLine("");
+                        Console.Write("You should try:");
+                        Console.WriteLine($" {movie.Title}");
+                        Console.WriteLine("Or");
+                        categoryFound = true;
+                    }
+                }
+
+                if (!categoryFound)
+                {
+                    Console.WriteLine("No movies found in that category.");
+                }
+
+                Console.Write("Do you want to search again? (y/n) ");
+                string continueInput = Console.ReadLine().ToLower();
+                if (continueInput != "y")
+                {
+                    //end
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
+
